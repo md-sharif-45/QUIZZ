dc4d254bbf3ca58a30ce182a4ab1ee6675e07f43
md-sharif-45 <mosharif5638@gmail.com>
2024-07-21 09:05:47 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,168 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What does JSON stand for?",
+    answer: "JavaScript Object Notation",
+    options: [
+      "JavaScript Online Notation",
+      "JavaScript Object Notation",
+      "JavaScript Open Notation",
+      "JavaScript Organizational Notation"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What does API stand for?",
+    answer: "Application Programming Interface",
+    options: [
+      "Application Programming Interface",
+      "Application Programming Integration",
+      "Application Performance Interface",
+      "Application Performance Integration"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What does HTTP stand for?",
+    answer: "Hypertext Transfer Protocol",
+    options: [
+      "Hypertext Transfer Protocol",
+      "Hypertext Transport Protocol",
+      "Hypertext Transfer Program",
+      "Hypertext Transport Program"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What does URL stand for?",
+    answer: "Uniform Resource Locator",
+    options: [
+      "Uniform Resource Locator",
+      "Universal Resource Locator",
+      "Uniform Resource Link",
+      "Universal Resource Link"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What does IP stand for?",
+    answer: "Internet Protocol",
+    options: [
+      "Internet Protocol",
+      "Internet Procedure",
+      "Internet Process",
+      "Internet Program"
+    ]
+  },
+  {
+    numb: 11,
+    question: "What does DNS stand for?",
+    answer: "Domain Name System",
+    options: [
+      "Domain Name System",
+      "Domain Name Service",
+      "Domain Number System",
+      "Domain Number Service"
+    ]
+  },
+  {
+    numb: 12,
+    question: "What does UX stand for?",
+    answer: "User Experience",
+    options: [
+      "User Experiment",
+      "User Expression",
+      "User Experience",
+      "User Extension"
+    ]
+  },
+  {
+    numb: 13,
+    question: "What does UI stand for?",
+    answer: "User Interface",
+    options: [
+      "User Interface",
+      "User Internet",
+      "User Internal",
+      "User Input"
+    ]
+  },
+  {
+    numb: 14,
+    question: "What does FTP stand for?",
+    answer: "File Transfer Protocol",
+    options: [
+      "File Transfer Protocol",
+      "File Transport Protocol",
+      "File Transmission Protocol",
+      "File Transform Protocol"
+    ]
+  },
+  {
+    numb: 15,
+    question: "What does SEO stand for?",
+    answer: "Search Engine Optimization",
+    options: [
+      "Search Engine Operation",
+      "Search Engine Optimization",
+      "Search Engine Organization",
+      "Search Engine Orchestration"
+    ]
+  }
+];
